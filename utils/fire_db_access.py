e8c59f06d891780328bb3c4ba4e3149ed0090b70
MelnykAndriy <kukoban@ukr.net>
2015-08-16 12:43:25 +0300

    Merge functionality with db logic into one file

--- utils/fire_db_access.py
+++ utils/fire_db_access.py
@@ -1,6 +1,8 @@
-
-import psycopg2
 import os
+import gdal
+import ogr
+import psycopg2
+import re
 
 DB_HOST = 'api.findafire.org'
 DB_NAME = 'findafire'
@@ -14,3 +16,49 @@ def get_fire_db_connection():
         password=os.getenv('FINDAFIRE_DB_PASSWORD')
     )
 
+
+def get_date_from_metadata(input_filename, band_number):
+    source_ds = gdal.Open(input_filename)
+    source_metadata = source_ds.GetMetadata()
+    get_date_as_string = source_metadata.get("DAYSOFYEAR")
+    date_list = re.findall(r"\d{4}\W\d{2}\W\d{2}", get_date_as_string)
+    return date_list[band_number - 1]
+
+# TODO only for testing
+DATE = "2015-01-01"
+AREA = 0
+DANGER_LEVEL = 0
+
+
+def insert_fire_polygons(input_filename):
+    source_ds = ogr.Open(input_filename)
+    source_layer = source_ds.GetLayer()
+
+    with get_fire_db_connection() as conn:
+        with conn.cursor() as cur:
+            #  reading all geometries
+            source_feature = source_layer.GetFeature(0)
+            while source_feature:
+                geom = source_feature.GetGeometryRef()
+                geom_center_point = geom.Centroid().ExportToWkt()
+                geom_export_wkt = geom.ExportToWkt()
+                geom_export_json = geom.ExportToJson()
+
+                insert_com = """INSERT INTO fire_places (date_of_fire, area, danger_level, center_point, shape, geojson)
+                                VALUES ('%(date)s', %(area)s, %(danger_level)s, ST_GeomFromText('%(center_point)s'),
+                                        ST_GeomFromText('%(shape)s'), '%(geojson)s' );
+                             """ % dict(
+                    date=DATE,
+                    area=AREA,
+                    danger_level=DANGER_LEVEL,
+                    center_point=geom_center_point,
+                    shape=geom_export_wkt,
+                    geojson=geom_export_json
+                )
+
+                cur.execute(insert_com)
+                source_feature.Destroy()
+                source_feature = source_layer.GetNextFeature()
+
+    source_layer = None
+    source_ds.Destroy()
