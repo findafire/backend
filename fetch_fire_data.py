a4d04b78d3361bfdbceed4a1795ca6bcc2a9ca1d
MelnykAndriy <kukoban@ukr.net>
2015-08-16 13:04:09 +0300

    Improve fetch script to work with any count of satellite

--- fetch_fire_data.py
+++ fetch_fire_data.py
@@ -1,4 +1,4 @@
-
+from functools import partial
 from utils.tiles_lookup import *
 import urllib
 import os.path
@@ -10,8 +10,8 @@ TILE_TMP_DIR = 'tmp/'
 ORIGINAL_TILE_SUFFIX = 'hdf'
 
 
-def get_last_added_tile_urls():
-    tiles_dirs = get_tiles_directories_urls(FIRE_DIRECTORY_URL)
+def get_last_added_tile_urls(satellite_directory_url):
+    tiles_dirs = get_tiles_directories_urls(satellite_directory_url)
     return get_tiles_files_urls(tiles_dirs[max(tiles_dirs.keys())])
 
 def transform_fire_file(tile_filename):
@@ -20,29 +20,35 @@ def transform_fire_file(tile_filename):
     os.remove(tile_filename)
     return transformed_filename
 
-def fetch_fire_tile(tile_url, tile_filename):
-    fetched_file_path = os.path.join(TILE_TMP_DIR, tile_filename)
+def fetch_fire_tile(tile_url, tile_filename, download_dir):
+    fetched_file_path = os.path.join(download_dir, tile_filename)
     urllib.urlretrieve(tile_url, fetched_file_path)
     return fetched_file_path
 
-def process_one_fire_tile(tile):
+def process_one_fire_tile(tile, download_dir):
     tile_filename, tile_url = tile
     try:
-        return transform_fire_file(fetch_fire_tile(tile_url, tile_filename))
+        return transform_fire_file(fetch_fire_tile(tile_url, tile_filename, download_dir))
     except IOError:
         pass
 
-def fetch_last_added_data():
+def fetch_last_added_satellite_data(satellite_fire_directory_ulr, tmp_dir):
 
-    if not (os.path.exists(TILE_TMP_DIR) and os.path.isdir(TILE_TMP_DIR)):
-        os.mkdir(TILE_TMP_DIR)
+    if not (os.path.exists(tmp_dir) and os.path.isdir(tmp_dir)):
+        os.mkdir(tmp_dir)
 
     p = Pool(4)
     fetched_files = filter(lambda f: f is None,
-                           p.map(process_one_fire_tile, get_last_added_tile_urls().items()))
+                           p.map(partial(process_one_fire_tile, download_dir=tmp_dir),
+                                 get_last_added_tile_urls(satellite_fire_directory_ulr).items()))
 
     intermediate_vrt_filename = '%s.vrt' % str(datetime.date.today())
     merge_to_vrt(intermediate_vrt_filename, fetched_files)
+
+
+def fetch_last_added_data():
+    fetch_last_added_satellite_data(FIRE_DIRECTORY_URL, 'satellite-data-tmp1')
+    # TODO fetch_last_added_satellite_data('url-to-another-satellite', 'satellite-data-tmp2')
 
 
 if __name__ == '__main__':
